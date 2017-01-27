ffb3b4d2e10ac2f53a4c70a7c93991e125a4d57f
Alex-Vasilev <aleksandr_vasilev1989@list.ru>
2017-01-27 20:31:32 +0300

    LinkedList

--- src/linked-list.js
+++ src/linked-list.js
@@ -9,141 +9,92 @@ const Node = require('./node');
         }
 
         append(data) {
-          return  this.insertAt(this.length, data);
-//        const node = new Node(data);
-//                console.log(list._tail instanceof Node);
-//                console.log(list._head instanceof Node);
-//                if (this._head === null) {
-//                 node.prev = node;
-//    node.next = node;
-//        this._head = node;
-//                this._tail = node;
-//        } else {
-//           node.prev = this._tail;
-//    node.next = this._head;
-//    this._head.prev = node;
-//    this._tail.next = node;
-//    this._tail = node;
+        return  this.insertAt(this.length, data);
         }
 
-//   if (this._head === null) {
-//     node.prev = node;
-//    node.next = node;
-//    this._head = node;
-//    this._tail = node;
-//  } else {
-//    node.prev = this._tail;
-//    node.next = this._head;
-//    this.head.prev = node;
-//    this.tail.next = node;
-//    this._tail = node;
-//  }
-//        this.length++;
-//        }
+        head() {
+        return this._head.next.data;
+                }
 
-head() {
-return this._head.next.data;
-}
+        tail() {
+        return this._tail.prev.data;
+                }
 
-tail() {
-return this._tail.prev.data;
-}
+        _at(index) {
+        var currentNode = this._head.next,
+                count = 0,
+                length = this.length;
+                if (isNaN(index) || index > length){
+        throw new Error(message.failure);
+                }
 
-_at(index) {
-var currentNode = this._head.next,
-        count = 0,
-        length = this.length;
-        if (isNaN(index) || index > length){
-throw new Error(message.failure);
-}
-
-while (count != index && count <= length) {
-currentNode = currentNode.next;
-        count++;
+        while (count != index && count <= length) {
+        currentNode = currentNode.next;
+                count++;
+        }
+        return currentNode;
         }
 
-return currentNode;
-//    console.log(currentNode.data)
+        at(index){
+        return this._at(index).data;
         }
 
-at(index){
-return this._at(index).data;
-        }
+        insertAt(index, data) {
+        var pos = this._at(index),
+                node = new Node(data, pos.prev, pos);
+                pos.prev = node;
+                node.prev.next = node;
+                this.length++;
+                return this;
+                }
 
-insertAt(index, data) {
-    var pos = this._at(index),
-            node = new Node(data, pos.prev, pos);
-    pos.prev = node;
-    node.prev.next = node;
-    this.length++;
-    return this;
-}
+        isEmpty() {
+        return this.length == 0 ? true : false;
+                }
 
-isEmpty() {
-//        return this.length == 0 ? true : false;
-}
-
-clear() {
-//        this.length = 0;
+        clear() {
+        this.length = 0;
 //        this._head = null;
 //        this._tail = null;
-}
+        }
 
-deleteAt(index) {
-//                    var currentNode = this._head,
-//        length = this.length,
-//        count = 0,
-//        message = {failure: 'Failure: non-existent node in this list.'},
-//        beforeNodeToDelete = null,
-//        nodeToDelete = null,
-//        deletedNode = null,
-//        afterNodeToDelete = null;
-//
-//    
-//    if (length === 0 || index < 0 || index > length) {
-//        throw new Error(message.failure);
-//    }
-//
-//    
-//    if (index === 0) {
-//        this._head = currentNode.next;
-//
-//        
-//        if (!this._head) {
-//            this._head.prev = null;
-//       
-//        } else {
-//            this._tail = null;
-//        }
-//
-//    
-//    } else if (index === this.length) {
-//        this._tail = this._tail.prev;
-//        this._tail.next = null;
-//    
-//    } else {
-//        while (count < index) {
-//            currentNode = currentNode.next;
-//            count++;
-//        }
-//
-//        beforeNodeToDelete = currentNode.prev;
-//        nodeToDelete = currentNode;
-//        afterNodeToDelete = currentNode.next;
-//
-//        beforeNodeToDelete.next = afterNodeToDelete;
-//        afterNodeToDelete.prev = beforeNodeToDelete;
-//        deletedNode = nodeToDelete;
-//        nodeToDelete = null;
-//    }
-//
-//    this.length--;
-}
+        deleteAt(index) {
+        var pos = this._at(index);
+                pos.next.prev = pos.prev;
+                pos.prev.next = pos.next;
+                this.length--;
+                pos.prev = null;
+                pos.next = null;
+                pos.data = null;
+                return this;
+                }
 
-reverse() {}
+        reverse() {
+        var currentHead = this._head.next,
+                currentTail = this._tail.prev,
+                index = 0,
+                temp;
+                while (index != Math.floor(this.index / 2)){
+        temp = currentHead.data;
+                currentHead.data = currentTail.data;
+                currentTail.data = temp;
+                currentHead = currentHead.next;
+                currentTail = currentTail.prev;
+        }
 
-indexOf(data) {}
-}
+        return this;
+                }
+
+        indexOf(data) {
+        var index = 0,
+                current = this._head.next;
+                while (current != null && current.data != data){
+        current = current.next;
+                index++;
+        }
+        return current === null ? - 1 : index;
+                }
+        }
 
 module.exports = LinkedList;
         const data = 42;
