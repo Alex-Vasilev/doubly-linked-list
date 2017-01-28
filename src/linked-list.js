f4ac0c195a8491742fbc7a08dc9a6b3e254806cc
Alex-Vasilev <aleksandr_vasilev1989@list.ru>
2017-01-28 17:56:03 +0300

    doubly-linked-list

--- src/linked-list.js
+++ src/linked-list.js
@@ -60,12 +60,12 @@ const Node = require('./node');
                 deleteAt(index);
                 index++
             }
-         this._head = new Node("HEAD", null, null);
-        this._tail = new Node("TAIL", null, null);
-        this._head.next = this._tail;
-        this._tail.prev = this._head;
-//            this._head = null;
-//            this._tail = null;
+//         this._head = new Node("HEAD", null, null);
+//        this._tail = new Node("TAIL", null, null);
+//        this._head.next = this._tail;
+//        this._tail.prev = this._head;
+            this._head = null;
+            this._tail = null;
             this.length=0;
             return this;
 //            console.log(this);
